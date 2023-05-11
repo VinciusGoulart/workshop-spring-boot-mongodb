499e420dc80bf3a2bbedafe5db45bfe537bd07fb
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-05-11 09:43:09 -0300

    Implemented findByTitle with query methods

--- src/main/java/com/vinicius/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/vinicius/workshopmongodb/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.vinicius.workshopmongodb.resources;
 
 import com.vinicius.workshopmongodb.domain.Post;
+import com.vinicius.workshopmongodb.resources.util.Url;
 import com.vinicius.workshopmongodb.servicies.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -24,4 +24,11 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam String text) {
+        text = Url.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
+    }
 }
